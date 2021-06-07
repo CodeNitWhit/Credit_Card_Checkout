e014bdc37804b7d6730157279e5dc68a3e7158d6
Codie Whitaker <CWhitaker2@student.fullsail.edu>
2021-06-07 17:00:48 -0400

    Unfeatured popup created and functional
    Main.js was created as well. Now ready to start doing some validation on the form fields, with some feedback for unsucessful(red) and successful(green) validations.

---
+++ main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("#pal-pay, #back-link").click(function() {
+        $("#unfeatured").fadeIn(300).delay(1000);
+        $("#unfeatured").fadeOut(300);
+    });
+});
